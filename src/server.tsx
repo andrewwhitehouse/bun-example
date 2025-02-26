248310f1c80f1db408fd06c642a04934d2a2ab2c
andrewwhitehouse <andrew-github@whitehouse.org.uk>
2025-02-26 16:22:42 +0000

    Adds static pages, which reference /version endpoint

---
+++ src/server.tsx
@@ -0,0 +1,13 @@
+import { type Context, Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
+
+const app = new Hono();
+
+// Serve static files from the public directory.
+app.use('/*', serveStatic({ root: './public' }));
+
+    app.get('/version', (c: Context) => {
+        return c.text(Bun.version);
+    });
+
+export default app;
